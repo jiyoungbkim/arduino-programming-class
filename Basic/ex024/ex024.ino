b835bab4127bccd4e07b73f3410512bc9d569a75
JiyoungKim <jiyoungbkim@gmail.com>
2018-12-07 12:29:22 +0900

    7세그먼트_자동카운트
    주석 추가

--- Basic/ex024/ex024.ino
+++ Basic/ex024/ex024.ino
@@ -12,6 +12,7 @@ int dig2 = 5;   // 좌측 기준으로 두번째 segment
 int dig3 = 6;   // 좌측 기준으로 세번째 segment
 int dig4 = A0;   // 좌측 기준으로 번째 segment
 
+// 숫자표시 배열
 byte digits[10][7] = {
   //{A, B, C, D, E, F, G}
   {1,1,1,1,1,1,0},  // 0
@@ -25,7 +26,7 @@ byte digits[10][7] = {
   {1,1,1,1,1,1,1},  // 8
   {1,1,1,1,0,1,1}   // 9
 };
-
+// 자리선택을 위한 배열
 int digitPos[4] = {dig1, dig2, dig3, dig4};
 
 int val = 0;
@@ -53,6 +54,7 @@ void setup() {
 }
 
 void loop() {
+  // 숫자의 자릿수를 구하기 위한 계산. 몫과 나머지이용
   val = millis() / 1000;
 
   val4 = (val / 1000) % 10;
@@ -90,19 +92,20 @@ void loop() {
     selectDigitPos(4);
     printNumber(val1);
     delayMicroseconds(1000);
-  }
-  
+  }  
 }
 
 void selectDigitPos(int pos) {
+  // HIGH로 전체 자리 신호를 끈다
   digitalWrite(dig1, HIGH);
   digitalWrite(dig2, HIGH);
   digitalWrite(dig3, HIGH);
   digitalWrite(dig4, HIGH);
-
+  // 인덱스 번호를 구하기 위해 1을 빼준다
   digitalWrite(digitPos[pos-1], LOW);
 }
 
+// 숫자 출력을 위한 함수
 void printNumber(int number) {
   digitalWrite(a_pin, digits[number][0]);
   digitalWrite(b_pin, digits[number][1]);
@@ -112,7 +115,29 @@ void printNumber(int number) {
   digitalWrite(f_pin, digits[number][5]);
   digitalWrite(g_pin, digits[number][6]);
 }
+/*byte digits[10][7] = {
+  //{A, B, C, D, E, F, G}
+  {1,1,1,1,1,1,0},  // 0
+  {0,1,1,0,0,0,0},  // 1
+  {1,1,0,1,1,0,1},  // 2
+  {1,1,1,1,0,0,1},  // 3
+  {0,1,1,0,0,1,1},  // 4
+  {1,0,1,1,0,1,1},  // 5
+  {1,0,1,1,1,1,1},  // 6
+  {1,1,1,0,0,0,0},  // 7
+  {1,1,1,1,1,1,1},  // 8
+  {1,1,1,1,0,1,1}   // 9
+};
+1을 출력한다고 했을 때
+  digitalWrite(a_pin, digits[1][0]); //(a_pin,0)
+  digitalWrite(b_pin, digits[1][1]); //(b_pin,1)
+  digitalWrite(c_pin, digits[1][2]); //(c_pin,1)
+  digitalWrite(d_pin, digits[1][3]); //(d_pin,0)
+  digitalWrite(e_pin, digits[1][4]); //(e_pin,0)
+  digitalWrite(f_pin, digits[1][5]); //(f_pin,0)
+  digitalWrite(g_pin, digits[1][6]); //(g_pin,0)*/
 
+// 출력을 초기화
 void clearAll() {
   digitalWrite(a_pin, LOW);
   digitalWrite(b_pin, LOW);
